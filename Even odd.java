ff23cfd256360280e1cbe4bae25a481f4ea6ebdb
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-24 18:07:13 +0530

    Add files via upload

---
+++ Even odd.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+
+        int x;
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter a number");
+        x = ob.nextInt();
+        if (x % 2 == 0) {
+            System.out.println(" It is an Even");
+ 
+        } else {
+            System.out.println("It is an Odd");
+        }
+
+    }
+}
