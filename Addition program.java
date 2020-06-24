98e0d1060ba9d2f996af32ae3d28fc1fbc092363
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-24 18:03:12 +0530

    Add files via upload

---
+++ Addition program.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+      
+        Scanner ob=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter 2 numbers ");
+        a=ob.nextInt();
+        b=ob.nextInt();
+        c=a+b;
+        
+        System.out.println("Addition is "+c);
+    }
+	
+}
