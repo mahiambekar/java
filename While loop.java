a72befc8bdbad1327c5bac5e160f47ef081489a6
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-28 19:48:18 +0530

    Add files via upload

---
+++ While loop.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		int i=0;
+		while(i<10){
+		    System.out.println("Hello");
+		    i++;
+		}
+	}
+}
