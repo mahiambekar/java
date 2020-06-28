2b74608e9a7fa85f715bec1a6482917ccf503692
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-28 18:12:29 +0530

    Add files via upload

---
+++ Taking output from keyboard.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name;
+		name=ob.next();
+		System.out.println("Hello"+name);
+	}
+}
