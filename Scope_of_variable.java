3cb24455cb0847e9cc96371d7c4c48dd764e6f1f
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-01 09:14:18 +0530

    Add files via upload

---
+++ Scope_of_variable.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+import static java.lang.Math.PI;
+
+class Test{
+    double PI=3.142;
+    void show(){
+        System.out.println(PI);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Test t1= new Test();
+		t1.show();
+	}
+}
