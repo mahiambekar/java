ed37ca4bec7a6369f8b81769a692a341b708bd86
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-02 16:56:33 +0530

    Add files via upload

---
+++ ThisKeyword.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Test{
+    int i;
+    Test(int i){
+        this.i=i;
+    }
+    void add(){
+        System.out.println(i+i);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Test t1=new Test(10);
+		t1.add();
+	}
+}
