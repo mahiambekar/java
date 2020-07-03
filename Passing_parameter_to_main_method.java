52d553ac145736c2876d6817cbe89939d1faf925
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-03 17:37:24 +0530

    Add files via upload

---
+++ Passing_parameter_to_main_method.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Test{
+    void show(String name){
+        System.out.println(name);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		  Test t1=new Test();
+		  t1.show("abc");
+	}
+}
