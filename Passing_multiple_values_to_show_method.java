cc0f406aa254357023084de82aa781f6b1f0a610
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-03 17:37:39 +0530

    Add files via upload

---
+++ Passing_multiple_values_to_show_method.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Test{
+    void show(String names[]){
+        for(String name:names){
+            System.out.println(name);
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	      String[] arr= ("a", "b", "c");
+		  Test t1=new Test();
+		  t1.show(arr);
+	}
+}
